b1712900dabd57ca7ca58510bf27ce0d40fedd3f
baron <baron@barons-MacBook-Pro.local>
2018-02-07 19:02:47 +0300

    [feature]Add routes for the links to the components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import React, { Component } from 'react';
+import _ from 'lodash'
 import logo from './logo.svg';
 import './App.css';
+import './static/styles.css'
+import TodoList from './components/todos-list'
+import CreateToDo from './components/create_todo'
+import Home from './components/home'
+import Register from './components/register'
+import Login from './components/login'
+import Dashboard from './components/dashboard'
+import Nav from './components/testnav'
+import Header from './components/header'
+import Footer from './components/footer'
+import List from './components/lists'
+import Items from './components/items'
+import EditList from './components/editlist'
+import EditItem from './components/edititem'
+import { BrowserRouter, Link, Route, Switch} from 'react-router-dom'
 
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+      <div>
+      <Header/>
+          <Switch>
+          <Route exact path="/" component={Login}/>
+          <Route exact path="/login" component={Login}/>
+          <Route exact path="/register" component={Register}/>
+          <Route exact path="/dashboard" component={Dashboard}/>
+          <Route exact path="/list" component={List}/>
+          <Route exact path="/items/:listId" component={Home}/>
+          <Route exact path="/editlist/:listId" component={EditList}/>
+          <Route exact path="/edititem/:listId/:itemId" component={EditItem}/>
+          </Switch>
+      <Footer/>
       </div>
+
+      
+      
     );
   }
+
 }
 
 export default App;
