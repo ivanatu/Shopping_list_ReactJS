57da3cc754bbf945de5deb48401e3428d6ed9213
baron <baron@barons-MacBook-Pro.local>
2018-02-13 00:13:54 +0300

    [feature #155144059]Modify edit component to models

--- src/components/edititem.js
+++ src/components/edititem.js
@@ -1,63 +1,98 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
+import {notify} from 'react-notify-toast';
 
 class EditItem extends Component{
+    
+
+    constructor(props) {
+        super(props)
+  
+        this.state = {
+            name:'',
+            price:'',
+        };
+    }
+
+    componentDidMount(){
+        this.setState({
+            name: this.props.itemName,
+            price: this.props.itemPrice,
+        })
+    }
 
     handleSubmit = (event) => {
         event.preventDefault();
        
-        const {match: {params}} = this.props;
-        const listId = params.listId;
-        const itemId = params.itemId;
-        console.log("this"+listId);
-        console.log("csdcssdcsdcsdc");
+        const {listId, itemId, itemName} = this.props;
         axios.put(`http://localhost:5000/shoppinglists/${listId}/items/${itemId}`,
-        {name:event.target.name.value, price:event.target.price.value}, 
+        {name:this.state.name, price:this.state.price}, 
         {headers: { 'Authorization': localStorage.getItem("TK") }})
           .then( (response) =>{
               console.log(response);
               console.log(response.data.name);
-              console.log("csdcssdcsdcsdc");
-               this.props.history.push(`/items/${listId}`)
+            document.querySelector(`#close${this.props.itemId}`).click();
+            this.props.getItems();
+            notify.show(response.data.message,"success", 4000);
             
           })
           .catch(function (error) {
-            console.log(error);
-          });
+            if(error.response){
+              const { data:{message} } = error.response;
+              notify.show(message, 'error', 5000)
+          }
+          }); 
         
+    }
+
+    handleNameChange = (event)=>{
+        const { value} = event.target;
+        this.setState({
+            name: value,
+        })
+    }
+
+    handlePriceChange = (event)=>{
+        const { value} = event.target;
+        this.setState({
+            price: value,
+        })
     }
 
     render(){
         return(
             <div>
            
-            <div class="container new_list_background card col-6">
-            
-            <h2 class="text-capitalize card-title mt-1">Edit List Item</h2>
-            <div class="row">
-                <div class="col">
-                  
-                    <form  method="POST"  name="create_form" class="register-form" onSubmit={this.handleSubmit}>
-
-                    <div class="form-group">
-                        NEW ITEM NAME:
-                            <input type="text" class="form-control" id="name" name="name"/>   
-                    </div>
-
-                     <div class="form-group">
-                       NEW ITEM PRICE:
-                            <input type="text" class="form-control" id="price" name="price"/>
-                    </div>
-                    <div class="form-group"> 
-                            <button class="btn btn-info " type="submit">Save Item</button>
-                            
-                    </div>
-                    
-                </form>
+            <div id={`myModals${this.props.itemId}`} className="modal" role="dialog">
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                <h4>Edit Item</h4>
+                  <button type="button" className="close" id={`close${this.props.itemId}`} data-dismiss="modal">&times;</button>
+                
                 </div>
-            </div>
-        </div>
+                <div className="modal-body">
+                <form  method="POST"  name="login_form" className="register-form" onSubmit={this.handleSubmit}>
+                
+                 <div className="form-group">
+                     NEW ITEM NAME:
+                         <input type="text" className="form-control" name="item" value={this.state.name} onChange={this.handleNameChange} required/>   
+                 </div>
+
+                 <div className="form-group">
+                     NEW ITEM PRICE:
+                     <input type="text" className="form-control" name="price" value={this.state.price} onChange={this.handlePriceChange} required min="0" step="1"/>  
+                 </div>
+       
+                         <button className="btn btn-info " type="submit">Save Item</button>
+                                    
+                  </form>
+                </div>
+                
+              </div>
+          
+          </div>
+       </div>
             </div>
         );
     }
