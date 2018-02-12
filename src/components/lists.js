4d7ab109bcf7dc7322461711375f42fd494d79fb
baron <baron@barons-MacBook-Pro.local>
2018-02-13 00:29:14 +0300

    [feature #155144059]Add pagination and search for the shopping lists

--- src/components/lists.js
+++ src/components/lists.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import _ from 'lodash'
-import cookie from 'react-cookie';
 import { confirmAlert } from 'react-confirm-alert'; 
-import { toast, ToastContainer } from 'react-toastify';
-import TodoListItem from './todo_list_item'
-import TodoHeader from './todos-list-header'
+import { toast} from 'react-toastify';
 import EditList from './editlist'
-import Items from './items'
+import {notify} from 'react-notify-toast';
 import 'react-confirm-alert/src/react-confirm-alert.css';
 
 
@@ -18,7 +14,22 @@ class List extends Component{
 
         this.state = {
         list: [],
-        isEditing: false
+        isEditing: false,
+        activePage: 1,
+        data:[],
+        pageNumbers:[],
+        showMessage:false, 
+        q:'', 
+        page:1, 
+        has_next:false, 
+        next_page:'',
+        previous_page:'', 
+        disablePrevious:'', 
+        disableNext:'',
+        // url: url+'?page=1',
+        pages:null,
+        per_page: '',
+        total: ''
         };
     }
     
@@ -41,48 +52,127 @@ class List extends Component{
         .then(response => {
             toast.success(response.data.message)
             // reload items state after delete
-            window.location.reload();
+            this.props.history.push('/dashboard')
         })
         .catch(error => {
         })
     }
-    componentDidMount(){
-       
-        axios.get('http://localhost:5000/shoppinglists', {
+
+    handleSearch = (event) => {
+        event.preventDefault();
+       const search = event.target.search.value
+       console.log("KABDKAJBSKDAKJSBD", search)
+        axios.get(`http://localhost:5000/shoppinglists?q=${search}`,{headers: { 'Authorization': localStorage.getItem("TK") }})
+          .then( (response) =>{
+             const list = response.data;
+              this.setState({list})
+            
+               console.log('list hrere',list)
+              this.props.history.push("/dashboard")
+            
+          })
+          .catch(function (error) {
+            if(error.response){
+              const { data:{message} } = error.response;
+              notify.show(message, 'error', 5000)
+          }
+          }); 
+        }
+
+    fetchLists = ()=>{
+            axios.get(`http://localhost:5000/shoppinglists`, {
             headers: { 'Authorization': localStorage.getItem("TK") }
         })
         .then(response => {
-            console.log(response.data);            
-            const list = response.data;
-               this.setState({list})
-               console.log('list ',list.lists[0])
+            console.log(response)
+            console.log(response.data.count)
+            this.setState({
+            list: response.data,
+            per_page: response.data.lists[0].per_page,
+            total: response.data.lists[0].total,
+            showMessage:false,
+            pages: response.data.count,
+            });
+            
+        })
+        .catch(function (error) {
+            if(error.response){
+              const { data:{message} } = error.response;
+              notify.show(message, 'error', 5000)
+          }
+          }); 
+    }
+
+    componentDidMount(){
+        this.fetchLists()
+    }
+
+    handleClick(number){
+        // if (this.state.next_page)
+        axios.get(`http://localhost:5000/shoppinglists?page=${number}`, {
+            headers: { 'Authorization': localStorage.getItem("TK") }
+        })
+        .then((response)=>{
+            console.log(response.data)
+            this.setState({
+            list: response.data,
+            showMessage:false,
+            });
 
         })
         .catch(function (error) {
-            console.log(error);
-          });
-    }
-
-    // renderTaskSection(){
-    //     const {task, isCompleted} = this.props;
-        
-
-    //     const taskStyle = {
-    //         color: isCompleted ? 'green' : 'red',
-    //         cursor: 'pointer'
-    //     };
-
-    // }
-    
-    
+            if(error.response){
+              const { data:{message} } = error.response;
+              notify.show(message, 'error', 5000)
+          }
+          }); 
+    };
 
 
     render () {
         
-        console.log(this.state);
+        console.log(this.state.per_page);
+        const {list, per_page, total}=this.state
+        let loadPagination;
+        const pageNumbers = [];
+       if(total > 8){
+           for (let i = 1; i <= Math.ceil(total / per_page); i++) {
+               pageNumbers.push(i);
+           }
+       } else {
+           pageNumbers.push(1);
+       }
+    
+       <div className="col-xs-11 col-sm-3 pull-right">
+        {
+            list?(
+            loadPagination = 
+            pageNumbers.map((number) => {
+            return(
+                <li className="page-item" key={number} style={{display: 'inline-block'}}>
+                <a className="page-link" onClick={() => this.handleClick(number)} key={number} id={number}>{number}</a>
+                </li> 
+            );  
+    }))
+       :null
+        }
+        </div>
+       
+   
         return (
+           <div>
+           <button type="button" className="btn btn-success" data-toggle="modal" data-target="#myModal">Add List</button>
+
+            <div className="form-inline align-content">
+            <form className="form-inline align-content" onSubmit = {this.handleSearch}>
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search" required/>
+           
+            <button  type="submit"><i className="fa fa-search"></i></button>
+            </form>
+            </div>
             
         <div className='container'>
+
         <table className="table table-hover table-striped">
         <thead>
         <tr>
@@ -97,10 +187,11 @@ class List extends Component{
                     return (          
             
          <tr key={list.id}>             
-                        <td>{list.list}</td>       
-                        <td><button onClick = {() => this.onEditClick(list.id)}>EDIT</button></td>
-                        <td><button onClick = {() => this.onDeleteClick(list.id, list.list)}>DELETE</button></td>
-                        <td><button onClick = {() => this.onListClick(list.id)}>VIEW ITEMS</button></td>
+                        <td>{list.list}</td>
+                        <td><i className="fa fa-edit" data-toggle="modal" data-target={`#myModall${list.id}`}></i></td>
+                        <td><i className="fa fa-trash" onClick = {() => this.onDeleteClick(list.id, list.list)}></i></td>
+                        <td><i className="fa fa-bars" onClick = {() => this.onListClick(list.id)}></i></td>
+                        <EditList listId={list.id} listName={list.list} getLists={this.fetchLists}/>
                     
         </tr>
          
@@ -112,14 +203,12 @@ class List extends Component{
                 }
                 </tbody> 
             </table>
-
+            <ul className="pagination justify-content-center">
+           {loadPagination}
+           </ul>
+        </div>
         </div>
         );
-    }
-
-
-    onEditClick = (listId) =>{
-        this.props.history.push(`/editlist/${listId}`)
     }
 
     onListClick = (listId) =>{
