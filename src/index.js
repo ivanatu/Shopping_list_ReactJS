2414358fbeebc36f651926381035bf85631c0dd1
baron <baron@barons-MacBook-Pro.local>
2018-02-13 00:35:15 +0300

    [feature #155144059]Add import for the notification css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "bootstrap/dist/css/bootstrap.css";
+import 'react-notifications/lib/notifications.css';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 import "bootstrap/dist/js/bootstrap.js";
 import './index.css';
 import App from './App';
