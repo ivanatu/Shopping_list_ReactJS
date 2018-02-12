8e625d357d669951d16e3423aee3454de4994866
baron <baron@barons-MacBook-Pro.local>
2018-02-13 00:31:59 +0300

    [feature #155144059]Modify the model for login

--- src/components/login.js
+++ src/components/login.js
@@ -1,70 +1,61 @@
 import React, { Component } from 'react';
-import Register from './register'
-import Header from './header'
-import Footer from './footer'
 import axios from 'axios';
-
-import {
-    BrowserRouter,
-    Route,
-    Link
-  } from 'react-router-dom'
+import {Link} from 'react-router-dom'
+import {notify} from 'react-notify-toast';
 
 class Login extends Component{
-    constructor (props) {
-        super (props);
-    }
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-       
-        axios.post('http://localhost:5000/auth/login', {
-            'email'    : event.target.email.value,
-            'password': event.target.password.value
-          })
-          .then( (response) =>{
-              console.log(response);
-              console.log(response.data.token)
-              localStorage.setItem("TK", response.data.token)
-
-              this.props.history.push("/dashboard")
-              
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-        
-    }
-    render(){
-        
-            return(
-                <div className="login">
-                    <div className="login-screen">
-                        <div className="app-title">
-                            <h1>Login</h1>
-                        </div>
+handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post(`http://localhost:5000/auth/login`, {
+        'email'    : event.target.email.value,
+        'password': event.target.password.value
+        })
+        .then( (response) =>{
+            console.log(response);
+            console.log(response.data.token)
+            localStorage.setItem("TK", response.data.token)
+            this.props.history.push("/dashboard")
+            notify.show(response.data.message, 'success', 500)
             
-                        <div className="login-form">
-                          <form method="POST"  name="login_form" className="register-form" onSubmit={this.handleSubmit}>
-                            <div className="control-group">
-                            <input type="text" className="login-field"  placeholder="email" name="email"/>
-                            <label className="login-field-icon fui-user" htmlFor="login-name"></label>
-                            </div>
+        })
+        .catch(function (error) {
+            if(error.response){
+              const { data:{message} } = error.response;
+              notify.show(message, 'error', 5000)
+          }
+          }); 
+        
+}
+render(){
+        
+return(
+    <div className="login">
+        <div className="login-screen">
+            <div className="app-title">
+                <h1>Login</h1>
+            </div>
             
-                            <div className="control-group">
-                            <input type="password" className="login-field"  placeholder="password" name="password"/>
-                            <label className="login-field-icon fui-lock" htmlFor="login-pass"></label>
-                            </div>
-                           <input type="submit" value="submit"/>
-                          </form>
-                          <Link to="/register"> Register new Account</Link>
-                             
-                        </div>
-                        
-                    </div>
+            <div className="login-form">
+                <form method="POST"  name="login_form" className="register-form" onSubmit={this.handleSubmit}>
+                <div className="control-group">
+                    <input type="text" className="login-field"  placeholder="email" name="email" required/>
+                    <label className="login-field-icon fui-user" htmlFor="login-name"></label>
                 </div>
-                );
-           
+
+                <div className="control-group">
+                    <input type="password" className="login-field"  placeholder="password" name="password" required/>
+                    <label className="login-field-icon fui-lock" htmlFor="login-pass"></label>
+                </div>
+                    <input type="submit" value="submit"/>
+                </form>
+                <Link to="/register"> Register new Account</Link>
+                    
+            </div>
+                        
+         </div>
+    </div>
+    );
     }
 }
 
