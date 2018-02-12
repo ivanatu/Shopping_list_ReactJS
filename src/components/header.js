1cc31ea6be8fbecda812a4385f5d8dbebc621109
baron <baron@barons-MacBook-Pro.local>
2018-02-13 00:16:59 +0300

    [feature #155144059]Add logout and url links for logout

--- src/components/header.js
+++ src/components/header.js
@@ -1,43 +1,47 @@
 import React, {Component} from 'react'
-import { BrowserRouter, Route, Link} from 'react-router-dom'
-import Login from './login'
-import Register from './register'
+import { Link} from 'react-router-dom'
+
 
 class Header extends Component{
+    
+
+    OnLogout(){
+        console.log(localStorage.getItem("TK"));
+        localStorage.removeItem("TK")
+        return(
+            this.props.history.push("/login")
+        );
+      }
+
     render(){
         return(
            
-            <div>
-                <nav className="navbar navbar-inverse fixed-top bg-faded navbar-toggleable-sm " >
-                    <div className="container">
-                        <h1 className="navbar-brand m-o align-content-end mr-auto">WELCOME TO YOUR SHOPPING LIST</h1>
-                        <form className="form-inline">
-                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <Link to="/search"><i className="fa fa-search" /></Link>
-                        </form>
-                        <div className="navbar-nav ">
-                              <div>
-                                <ul>
-                                  <Link to="/"><i className="fa fa-home" />  Home   </Link>
-                                  { localStorage.getItem("TK") ?
-                                  
-                                  <Link to="/logout"><i className="fa fa-sign-in" />  Logout   </Link>
+        <div>
+            <nav className="navbar navbar-inverse fixed-top bg-faded navbar-toggleable-sm " >
+                <div className="container">
+                    <h1 className="navbar-brand m-o align-content-end mr-auto">WELCOME TO YOUR SHOPPING LIST</h1>
+                    
+                    <div className="navbar-nav ">
+                            <div>
+                            <ul>
                                 
-                                  :
-                                   <div>
-                                  <Link to="/login"><i className="fa fa-sign-in" />  Login   </Link>
-                                  <Link to="/register"><i className="fa fa-sign-in" />   Register  </Link></div>
-                                }
-                                </ul>
-                          
-                                <hr/>
-                              </div>
-                          
-                        </div>
+                                { localStorage.getItem("TK") ?
+                                <div>
+                                <Link to="/dashboard"><i className="fa fa-home" />  Home   </Link>
+                                <Link to="/login" onClick={this.OnLogout}><i className="fa fa-sign-in" />  Logout  </Link></div>
+                                :
+                                <div>
+                                <Link to="/login"><i className="fa fa-sign-in" />  Login   </Link>
+                                <Link to="/register"><i className="fa fa-sign-in" />   Register  </Link></div>
+                            }
+                            </ul>
+                            <hr/>
+                            </div>
                     </div>
-                </nav>
-             
                 </div>
+            </nav>
+            
+            </div>
                
         );
     }
