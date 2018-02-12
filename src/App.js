781854fb1ae40e0a5a733c1a2ce18dff113ae79a
baron <baron@barons-MacBook-Pro.local>
2018-02-12 23:11:10 +0300

    [feature #155144059]Add react notify for message notifications

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
-import _ from 'lodash'
-import logo from './logo.svg';
 import './App.css';
 import './static/styles.css'
-import TodoList from './components/todos-list'
-import CreateToDo from './components/create_todo'
 import Home from './components/home'
 import Register from './components/register'
 import Login from './components/login'
 import Dashboard from './components/dashboard'
-import Nav from './components/testnav'
 import Header from './components/header'
 import Footer from './components/footer'
 import List from './components/lists'
-import Items from './components/items'
 import EditList from './components/editlist'
 import EditItem from './components/edititem'
-import { BrowserRouter, Link, Route, Switch} from 'react-router-dom'
+import Notifications from 'react-notify-toast';
+import { Route, Switch} from 'react-router-dom'
 
 
 class App extends Component {
@@ -25,6 +20,7 @@ class App extends Component {
     return (
 
       <div>
+      <Notifications options={{zIndex: 5000}}/>
       <Header/>
           <Switch>
           <Route exact path="/" component={Login}/>
@@ -33,14 +29,11 @@ class App extends Component {
           <Route exact path="/dashboard" component={Dashboard}/>
           <Route exact path="/list" component={List}/>
           <Route exact path="/items/:listId" component={Home}/>
-          <Route exact path="/editlist/:listId" component={EditList}/>
+          <Route exact path="/editlist/:listId/:listName" component={EditList}/>
           <Route exact path="/edititem/:listId/:itemId" component={EditItem}/>
           </Switch>
       <Footer/>
       </div>
-
-      
-      
     );
   }
 
