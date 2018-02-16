463692a8fe53fbde6690e608a61044440b3d1bf4
baron <baron@barons-MacBook-Pro.local>
2018-02-16 16:57:18 +0300

    [feature #155144059]Add more tests for the header file

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,20 +1,53 @@
 import React from 'react';
 import { shallow, render, mount } from 'enzyme';
 import ReactDOM from 'react-dom' 
+import renderer from 'react-test-renderer'
 import Header from '../components/header';
-// import '../setupTests';
+import '../setupTests';
 
 
 describe('<Header/>', () => {
-    // const header = shallow( <Header/> );
-    // // it('should render nav without throwing an error', () => {
-    // //     expect(header.exists(<nav className="nav bar"/>))
-    // // });
+     const wrapper = shallow( <Header/> );
+    it('renders without crashing', () => {
+        shallow(<Header/>);
+    });
+    it('renders with a snapshot', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+    it('should render nav without throwing an error', () => {
+        expect(wrapper.exists(<nav className="nav bar"/>)).toBe(true)
+    });
     it('should render <div> without throwing an error', () => {
         expect(wrapper.exists(<div className="container" />)).toBe(true)
     });
-    // it('should render a link', () =>{
-    //     expect(wrapper.find('Link').length).toEqual(1)
-    // })
+    it('should render <h1> without throwing an error', () => {
+        expect(wrapper.exists(<h1 className="navbar-brand m-o align-content-end mr-auto"/>)).toBe(true)
+    });
+    it('should render <div> without throwing an error', () => {
+        expect(wrapper.exists(<div className="navbar-nav" />)).toBe(true)
+    });
+    it('should render a link', () =>{
+        expect(wrapper.find('Link').length).toEqual(2)
+    })
 });
+
+// describe('Header.OnLogout', () => {
+//     const props = {
+//         history : {
+//             push: {
+//                 listId: 4,
+//             }
+//         }
+//     };
+//     it('returns true when called', () => {
+//         const wrapper = shallow(<Header />);
+//         const instance = wrapper.instance();
+
+//         // spy on the instance instead of the component
+//         spyOn(instance, 'OnLogout').and.callThrough();
+
+//         expect(instance.OnLogout()).toBe(true);
+//         expect(instance.OnLogout).toHaveBeenCalled();
+//     });
+// });
 
