0949f935fa2e90bc5d7e9ef048b141663f4dd54f
baron <baron@barons-MacBook-Pro.local>
2018-02-19 00:38:28 +0300

    [feature #155144059]Add state handlers for the input states

--- src/components/register.js
+++ src/components/register.js
@@ -18,8 +18,14 @@ class Register extends Component{
         super (props);
 
         this.handleSubmit = this.handleSubmit.bind(this)
-        this.state = {login: false}
-
+        this.state = {
+            login: false,
+            first_name:'',
+            last_name:'',
+            email:'',
+            password:'',
+            c_password:''
+        };
     }
 
     showLogin = (props) => {
@@ -36,15 +42,21 @@ handleSubmit = (event) => {
     }
     
     axios.post(`/auth/register`, {
-        'first_name': event.target.first_name.value,
-        'last_name': event.target.last_name.value,
-        'email'    : event.target.email.value,
-        'password': event.target.password.value
+        first_name: this.state.first_name,
+        last_name: this.state.last_name,
+        'email'    : this.state.email,
+        'password': this.state.password
         })
         .then((response) =>{
             console.log(response);
             notify.show(response.data.message, "error", 4000)
-        this.props.history.push("/login")   
+        this.props.history.push("/login") 
+        this.setState({
+            first_name:'',
+            last_name:'',
+            email:'',
+            password:'',
+        })  
         })
         .catch(function (error) {
         if(error.response){
@@ -55,6 +67,13 @@ handleSubmit = (event) => {
         });      
     }
 
+handleChange = (event)=>{
+    const {value,name} = event.target;
+    this.setState({
+        [name]: value,
+    })
+}
+
 render(){
     return (   
     <div>   
@@ -63,42 +82,42 @@ render(){
                 <div className=" login-screen">
                     <h2 className="text-capitalize card-title mt-5">Register</h2>
                     <div className=" login-form">
-                        <form method="POST" name="login_form" className="register-form" onSubmit={this.handleSubmit}>
+                        <form method="POST" id="login_form" name="login_form" className="register-form" onSubmit={this.handleSubmit}>
                     <div className="row signupForm">
                         <div className="col">
 
                             <div className="control-group">
                                 <label className="control-label">Firstname</label>
                                 <div className="col">
-                                    <input type="text" className="form-input form-control" name="first_name" required/>
+                                    <input type="text" className="form-input form-control" id="first_name" name="first_name" value={this.state.first_name} onChange={this.handleChange} required/>
                                 </div>
                             </div>
        
                             <div className="control-group">
                                 <label className="control-label">Lastname</label>
                                 <div className="col">
-                                     <input type="text" className="form-input form-control" name="last_name" required/>
+                                     <input type="text" className="form-input form-control" id="last_name" name="last_name" value={this.state.last_name} onChange={this.handleChange}required/>
                                 </div>
                             </div>
         
                             <div className="control-group">
                                 <label className="control-label align-content-around" htmlFor="email">Email</label>
                                 <div className="col">
-                                    <input type="email" className="form-control" name="email" required/>
+                                    <input type="email" className="form-control" id="email" name="email" value={this.state.email} onChange={this.handleChange}required/>
                                 </div>
                             </div>
         
                             <div className="control-group">
                                 <label className="control-label align-content-around" htmlFor="password">Password</label>
                                 <div className="col">
-                                    <input type="password" className="form-input form-control" name="password" required minLength="8"/>
+                                    <input type="password" className="form-input form-control" id="password" name="password" value={this.state.password} onChange={this.handleChange} required minLength="8"/>
                                 </div>
                             </div>
 
                             <div className="control-group">
                                 <label className="control-label align-content-around" htmlFor="password">Confirm Password</label>
                                 <div className="col">
-                                    <input type="password" className="form-input form-control" name="c_password" required minLength="8"/>
+                                    <input type="password" className="form-input form-control" id="c_password" name="c_password" value={this.state.c_password} onChange={this.handleChange} required minLength="8"/>
                                 </div>
                            </div>
                             <button type='submit' className="btn btn-primary">Submit</button>
