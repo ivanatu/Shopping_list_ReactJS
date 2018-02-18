fea93acb1a9cc4d52c0e9df392a8bf4b4d621a40
baron <baron@barons-MacBook-Pro.local>
2018-02-19 00:39:09 +0300

    [feature #155144059]Remove unnecessary imports

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import 'react-confirm-alert/src/react-confirm-alert.css';
 import "bootstrap/dist/js/bootstrap.js";
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter} from 'react-router-dom'
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
