f0b008b4fdacc70ffab1f1231572e470d41b5378
baron <baron@barons-MacBook-Pro.local>
2018-02-19 00:25:08 +0300

    [feature #155144059]Add more tests for the edit item component

--- src/__tests__/Edititem.test.js
+++ src/__tests__/Edititem.test.js
@@ -3,10 +3,14 @@ import { shallow, render, mount } from 'enzyme';
 import ReactDOM from 'react-dom' 
 import renderer from 'react-test-renderer'
 import Edititem from '../components/edititem'
+import toJson from 'enzyme-to-json';
 import '../setupTests';
+
+
 
 describe('<Edititem/>', () => {
     const wrapper = shallow( <Edititem/> );
+    
     it('has a valid snapshot', () => {
         const component = renderer.create(
         <Edititem />);
@@ -14,7 +18,8 @@ describe('<Edititem/>', () => {
         expect(tree).toMatchSnapshot();
     });
     it('renders without crashing', () => {
-        shallow(<Edititem/>);
+        render(<Edititem/>);
+        expect(wrapper).toMatchSnapshot();
     });
     it('should render <div> without throwing an error', () => {
         expect(wrapper.exists(<div className="modal"/>))
@@ -40,5 +45,19 @@ describe('<Edititem/>', () => {
     it('render the inputs', () =>{
         expect(wrapper.find('form').length).toEqual(1);
         expect(wrapper.find('input').length).toEqual(2)
+    });
+    it('input should respond to change event and change the state', () => {
+        wrapper.find('#name').simulate('change', { target: { name: 'name', value: 'apples' } });
+        expect(wrapper.state().name).toEqual('apples')
+    });
+    it('input should respond to change event and change the state', () => {
+        wrapper.find('#price').simulate('change', { target: { name: 'price', value: '20' } });
+        expect(wrapper.state().price).toEqual('20')
+    });
+    it('renders the edit item form and submits data', () =>{
+        wrapper.setState({name:'apples', price:'20'});
+        wrapper.find("#edititem").simulate('submit', {preventDefault(){}});
+        expect(toJson(wrapper)).toMatchSnapshot();
+        // expect(handleSubmit.calledOnce).toEqual(false)
     });
 });
